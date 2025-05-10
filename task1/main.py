a4987d250ee83d0c6d0e921f46b50b2467ff63a0
julpo99 <julpo99@icloud.com>
2025-05-10 15:09:40 +0200

    EDIT: main.py added utf-8 encoding

--- task1/main.py
+++ task1/main.py
@@ -20,7 +20,7 @@ def save_output(timestamp: float, data: Dict[str, Any], output_dir: str = "outpu
     """
     Path(output_dir).mkdir(exist_ok=True)
     fname = f"{output_dir}/prices_{timestamp}.json"
-    with open(fname, "w") as f:
+    with open(fname, "w", encoding="utf-8") as f:
         json.dump(data, f, indent=2)
 
 
